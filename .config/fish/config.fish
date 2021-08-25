885c3edd89644ab8dad5f73188dd2cd4662504bc
Arsenii Liunsha <me@arslee.dev>
2021-08-25 14:10:34 +0900

    add starship config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,25 @@
 # Set TrueColor support
-set -g default-terminal "xterm-256color"
-set -ga terminal-overrides ",*256col*:Tc"
+#set -g default-terminal "xterm-256color"
+#set -g terminal-overrides ",*256col*:Tc"
 
 # Hide fish greeting
 set fish_greeting
 
 # Some useful aliases
 alias del="trash"
-alias sai="sudo apt install"
-alias sau="sudo apt update"
 alias c="clear"
 alias fconf="nano ~/.config/fish/config.fish"
 alias tconf="nano ~/.tmux.conf"
 alias vconf="nano ~/.config/nvim/init.vim"
 
+# Debian-based distros only
+alias sai="sudo apt install"
+alias sau="sudo apt update"
+
 # Fish command history
 function history
     builtin history --show-time='%F %T '
 end
-
-# Bobthefish settings
-set -g theme_color_scheme dracula
-set -g theme_date_format "+%a %H:%M"
-set -g theme_display_user ssh
-set -g theme_display_hostname no
 
 # Easy file backup
 function backup --argument filename
@@ -42,7 +38,17 @@ alias l.="exa -a | egrep '^\.'"                                     # show only 
 alias du="dust"
 
 # Replace cat with bat
-alias cat="batcat"
+alias cat="bat"
+
+# Some Dart specific exports
+export PATH="$PATH:/home/arslee/flutter/bin"
+export CHROME_EXECUTABLE="/usr/bin/google-chrome-stable"
+export PATH="$PATH":"/home/arslee/.pub-cache/bin"
+
+# Set TrueColor support for Micro
+export MICRO_TRUECOLOR=1
+
+# Init Starship prompt
+starship init fish | source
 
 clear
-PF_COL1=4 PF_COL2=4 PF_COL3=4 pfetch
