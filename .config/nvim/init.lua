5cc3a1f95365140f40281a4eea6ed77a046a12ca
arslee <me@arslee.dev>
2022-01-24 22:52:10 +0900

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,51 +3,52 @@ local opt = vim.opt
 local g = vim.g
 
 require('packer').startup(function()
-  -- Autocompletion
-  use 'neovim/nvim-lspconfig'
-  
   use 'wbthomason/packer.nvim'  -- Package manager
-  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}  -- Treesitter support
-  --use 'kyazdani42/nvim-tree.lua' -- File explorer
+  
+  -- API
+  use 'nvim-lua/plenary.nvim' -- i forgor :skull:
+  use 'kyazdani42/nvim-web-devicons' -- Devicons support
   use 'b0o/mapx.nvim' -- Better keymapping util
+  use {'nvim-treesitter/nvim-treesitter', run = ':TSUpdate'}  -- Treesitter support
+  
+  -- Completions
+  use 'neovim/nvim-lspconfig' -- Preconfigured LSPs
+  use 'hrsh7th/nvim-cmp' -- Completions engine
+  use 'hrsh7th/cmp-nvim-lsp' --Neovim builtin LSP support for cmp
+  use 'hrsh7th/cmp-vsnip' -- VSnip support for cmp
+  use 'hrsh7th/cmp-buffer' -- ...buffers
+  use 'hrsh7th/cmp-path' -- ...paths
+  use 'hrsh7th/cmp-cmdline' -- ...commands
+  use 'hrsh7th/vim-vsnip' -- Snippets engine
+  
+  -- Navigation
+  use 'kyazdani42/nvim-tree.lua' -- File explorer
+  use 'cloudhead/neovim-fuzzy' -- Fuzzy file search (TODO: replace with telescope?)
+
+  -- Coding QoL
+  use 'lewis6991/gitsigns.nvim' -- Git integration for buffers
+  use 'lukas-reineke/indent-blankline.nvim' -- Indent lines
+  use 'p00f/nvim-ts-rainbow' -- Rainbow brackets
   use 'windwp/nvim-autopairs' -- Autopair
+
+  -- Language specific
   use 'dart-lang/dart-vim-plugin' -- Dart syntax highlight etc
-  use 'lukas-reineke/indent-blankline.nvim'
-  use 'p00f/nvim-ts-rainbow'
-  use 'cloudhead/neovim-fuzzy'
-  use 'andweeb/presence.nvim' -- Discord presence
+  
+  -- Appearance
   use 'Mofiqul/dracula.nvim' -- Dracula theme
+  use 'noib3/nvim-cokeline' -- Bufferline
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+
+  -- Other
+  use 'andweeb/presence.nvim' -- Discord presence
 end)
 
 cmd('PackerInstall')
 
 require('nvim-autopairs').setup()
 require('mapx').setup{global="force"}
---require('nvim-tree').setup()
-vim.g.nvim_tree_icon_padding = ''
-vim.g.nvim_tree_icons = {
-    default = '',
-    symlink = '>',
-    git = {
-      unstaged =  "",
-      staged = "",
-      unmerged = "",
-      renamed = "",
-      untracked = "",
-      deleted = "",
-      ignored = ""
-     },
-    folder = {
-      arrow_open = "",
-      arrow_closed = "",
-      default = "+",
-      open = "-",
-      empty = "+",
-      empty_open = "-",
-      symlink = ">",
-      symlink_open = "v",
-      }
-    }
+require('nvim-tree').setup()
+require('gitsigns').setup()
 require("indent_blankline").setup {
     space_char_blankline = " ",
     show_current_context = true,
@@ -60,6 +61,70 @@ require("nvim-treesitter.configs").setup {
     max_file_lines = nil,
   }
 }
+local get_hex = require('cokeline/utils').get_hex
+
+require('cokeline').setup({
+  default_hl = {
+    focused = {
+      fg = get_hex('Normal', 'fg'),
+      bg = 'NONE',
+    },
+    unfocused = {
+      fg = get_hex('Comment', 'fg'),
+      bg = 'NONE',
+    },
+  },
+
+  rendering = {
+    left_sidebar = {
+      filetype = 'NvimTree',
+      components = {
+        {
+          text = '  NvimTree',
+          hl = {
+            fg = yellow,
+            bg = get_hex('NvimTreeNormal', 'bg'),
+            style = 'bold'
+          }
+        },
+      }
+    },
+  },
+
+  components = {
+    {
+      text = function(buffer) return ' ' .. buffer.devicon.icon end,
+      hl = {
+        fg = function(buffer) return buffer.devicon.color end,
+      },
+    },
+    {
+      text = function(buffer) return buffer.unique_prefix end,
+      hl = {
+        fg = get_hex('Comment', 'fg'),
+        style = 'italic',
+      },
+    },
+    {
+      text = function(buffer) return buffer.filename .. ' ' end,
+    },
+  },
+})
+require('lualine').setup {
+  options = {
+    component_separators = { left = '|', right = '|'},
+    section_separators = { left = '', right = ''},
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  extensions = {'nvim-tree', 'quickfix'}
+}
 
 -- LSP SETUP --
 vim.diagnostic.config({
@@ -70,14 +135,73 @@ vim.diagnostic.config({
   severity_sort = false,
 })
 
-local on_attach = function(client, bufnr)
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
+
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
+local cmp = require'cmp'
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  mapping = {
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn["vsnip#available"](1) == 1 then
+        feedkey("<Plug>(vsnip-expand-or-jump)", "")
+      elseif has_words_before() then
+        cmp.complete()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function()
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+        feedkey("<Plug>(vsnip-jump-prev)", "")
+      end
+    end, { "i", "s" }),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  },
+  completion = {
+    autocomplete = false,
+  },
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'vsnip' }, 
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+cmp.setup.cmdline('/', {
+  sources = {
+    { name = 'buffer' }
+  }
+})
+cmp.setup.cmdline(':', {
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Setup lspconfig.
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 local servers = {'dartls', 'pylsp', 'gopls'}
 for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup {on_attach=on_attach, flags={debounce_text_changes = 150}}
+  require('lspconfig')[lsp].setup {on_attach=on_attach, capatibilities=capatibilities, flags={debounce_text_changes = 150}}
 end
 ---
 
@@ -103,86 +227,7 @@ noremap('<Down>', 'gj')
 inoremap('<Up>', '<C-o>gk')
 inoremap('<Down>', '<C-o>gj')
 
-cmd [[function! InsertTabWrapper()
-  if pumvisible()
-    return "\<c-n>"
-  endif
-  let col = col('.') - 1
-  if !col || getline('.')[col - 1] !~ '\k'
-    return "\<tab>"
-  else
-    return "\<c-x>\<c-o>"
-  endif
-endfunction]]
-
-inoremap('<Tab>', '<C-r>=InsertTabWrapper()<CR>')
-
 cmd('colorscheme dracula')
-
--- Statusline setup
--- TODO: rewrite to lua
-cmd[[
-set nocompatible
-set laststatus=2
-set statusline=
-
-hi User1 guibg=#44475A guifg=f8f8f2
-hi User9 guibg=#3A3C4F guifg=f8f8f2
-
-" Left side
-set statusline+=%1*\ 
-set statusline+=%{StatuslineMode()}
-set statusline+=\ %9*
-set statusline+=\ 
-set statusline+=%f " Filename
-set statusline+=\ 
-set statusline+=%m " Is modified
-set statusline+=\ 
-set statusline+=%r " Is readonly
-
-set statusline+=%=
-
-set statusline+=%1*\ 
-set statusline+=%P " File percentage
-set statusline+=\ 
-set statusline+=%l " Current line
-set statusline+=:
-set statusline+=%c " Current column
-set statusline+=\ 
-
-function! StatuslineMode()
-  let l:mode=mode()
-  if l:mode==#"n"
-    return "NORMAL"
-  elseif l:mode==?"v"
-    return "VISUAL"
-  elseif l:mode==#"i"
-    return "INSERT"
-  elseif l:mode==#"R"
-    return "REPLACE"
-  elseif l:mode==?"s"
-    return "SELECT"
-  elseif l:mode==#"t"
-    return "TERMINAL"
-  elseif l:mode==#"c"
-    return "COMMAND"
-  elseif l:mode==#"!"
-    return "SHELL"
-  endif
-endfunction
-]]
-
-cmd[[
-nnoremap <silent> ge :Lexplore<cr>
-vnoremap <silent> ge :Lexplore<cr>
-
-let g:netrw_altv=1
-let g:netrw_banner=0
-let g:netrw_winsize=30
-let g:netrw_liststyle=3
-let g:netrw_browse_split=0
-let g:netrw_list_hide='.*\.git/$,'.netrw_gitignore#Hide()
-]]
 
 g.fuzzy_rootcmds = {{"git", "rev-parse", "--show-toplevel"}}
 opt.ts = 4
